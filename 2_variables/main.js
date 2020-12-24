c002ef37fb479371bb3e36741377022efb6502f6
dmitry-shishkov <shishkovdmitry@yandex.ru>
2020-12-24 11:12:46 +0300

    added files

---
+++ 2_variables/main.js
@@ -0,0 +1,52 @@
+
+// Модуль 2 Задача 1
+
+// let x1 = 2;
+// let y1 = 3;
+
+// let x2 = 10;
+// let y2 = 5;
+
+// let widthRect = Math.abs(x1 - x2);
+// let heightRect = Math.abs (y1 - y2);
+// let areaRect = widthRect * heightRect;
+
+
+// console.log('Площадь прямоугольника равна ' + areaRect);
+
+
+
+// Модуль 2 Задача 2
+
+// let a = 13.123456789;
+// let b = 2.123;
+// let n = 5;
+
+// let aFractionPart = Math.floor((a.toFixed(n) - Math.floor(a)) * Math.pow( 10, n));
+// let bFractionPart = Math.floor((b.toFixed(n) - Math.floor(b)) * Math.pow( 10, n));
+
+// console.log('Дробная часть числа ' + a +' -', aFractionPart);
+// console.log('Дробная часть числа ' + b +' -', bFractionPart);
+// console.log(aFractionPart + ' > ' + bFractionPart, aFractionPart > bFractionPart);
+// console.log(aFractionPart + ' < ' + bFractionPart, aFractionPart < bFractionPart);
+// console.log(aFractionPart + ' >= ' + bFractionPart, aFractionPart >= bFractionPart);
+// console.log(aFractionPart + ' <= ' + bFractionPart, aFractionPart <= bFractionPart);
+// console.log(aFractionPart + ' === ' + bFractionPart, aFractionPart === bFractionPart);
+// console.log(aFractionPart + ' != ' + bFractionPart, aFractionPart != bFractionPart);
+
+
+
+// Модуль 2 Задача 3
+
+// let n = -3;
+// let m = -10;
+// let rangeValues = m - n;
+
+// let random = Math.round(Math.random() * rangeValues) + n;
+// let randomOdd = random + 1;
+
+//   if (Math.abs(random)%2 === 1) {
+//     console.log(random);
+//   } else {
+//     console.log(randomOdd);
+//   }
---
+++ 3_string/main.js
@@ -0,0 +1,48 @@
+
+// Модуль_3 Задача_1
+
+// let password = '123456789_';
+
+//   if ( (password.includes('-') || password.includes('_')) && (password.length > 3 && password.length < 27) ) {
+//     console.log('Пароль надёжный');
+//   } else {
+//     console.log('Пароль недостаточно надёжный');
+//   }
+
+// Модуль_3 Задача_2
+
+// let name = 'ИмЯ';
+// let surname = 'ФаМиЛия';
+
+// let nameFirstLetter = name[0].toUpperCase();
+// let nameEdge = name.substr(1);
+// let nameLowerCase = nameEdge.toLowerCase();
+// let nameOrdered = nameFirstLetter + nameLowerCase;
+
+// let surnameFirstLetter = surname[0].toUpperCase();
+// let surnameEdge = surname.substr(1);
+// let surnameLowerCase = surnameEdge.toLowerCase();
+// let surnameOrdered = surnameFirstLetter + surnameLowerCase;
+
+// let nameComparison = name.localeCompare(nameOrdered);
+// let surnameComparison = surname.localeCompare(surnameOrdered);
+
+// console.log(`Введенное имя: ${name}
+// Измененное имя: ${nameOrdered}
+
+// Введенная фамлия: ${surname}
+// Измененная фамилия: ${surnameOrdered}
+
+// `);
+
+//   if (nameComparison == 0) {
+//     console.log('Имя осталось без изменений');
+//   } else {
+//     console.log('Имя преобразовано');
+//   }
+
+//   if (surnameComparison == 0) {
+//     console.log('Фамилия осталась без изменений');
+//   } else {
+//     console.log('Фамилия преобразована');
+//   }
